090c25ebd5a36995018e9dffc984ba28ebcf61c4
João <jgablopes2008@gmail.com>
2025-07-27 09:14:48 -0300

    Primeiro commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+using MinimalApi.DTOs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapGet("/", () => "Hello World!");
+
+app.MapPost("/login", (LoginDTO loginDTO) =>
+{
+    if (loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456")
+    {
+        return Results.Ok("Login feito com sucesso");
+    }
+    else
+    {
+        return Results.Unauthorized();
+    }
+});
+
+app.Run();
+
